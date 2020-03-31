ab175342c90dd620a9b44cbe8e03cf247bfd6cab
Sandyl Nursigadoo <sandylnursigadoo@outlook.com>
2020-03-31 18:26:56 +0400

    Create Base FIle For Development

---
+++ Arduino/1_Arduino-Main/2_Setup-Function.ino
@@ -0,0 +1,9 @@
+void setup() {
+/* 
+    Arduino Setup Function
+    #run setup code once
+*/
+
+// put your setup code here, to run once:
+
+}
---
+++ Arduino/1_Arduino-Main/3_Loop-Function.ino
@@ -0,0 +1,9 @@
+/* 
+    Arduino Loop Function
+    #run main code repeatedly
+*/
+
+void loop() {
+    // put your main code here, to run repeatedly:
+    
+}
